86e8e02543b24a677260e6592a3663674cb1f28c
Suresh Mahalingam <msuresh@cheerful.com>
2014-05-11 12:10:29 +0000

    Added Load test to run the ADD_SCRIPT several times in a pipeline.

--- test/org/aredis/test/cache/TestScript.java
+++ test/org/aredis/test/cache/TestScript.java
@@ -1,5 +1,7 @@
 package org.aredis.test.cache;
 
+import java.util.concurrent.Future;
+
 import org.aredis.cache.AsyncRedisClient;
 import org.aredis.cache.AsyncRedisFactory;
 import org.aredis.cache.RedisCommand;
@@ -9,7 +11,74 @@ import org.aredis.cache.Script;
 
 public class TestScript {
 
+    // Below is for performance test of the simple ADD_SCRIPT with pipelined commands 2000 at a time
     public static void main(String [] args) throws Exception {
+        AsyncRedisFactory f = new AsyncRedisFactory(null);
+        // The below call returns the same AsyncRedisClient for all calls to the same server
+        AsyncRedisClient aredis = f.getClient("my.redis.server2");
+        int i, j, count = 0, pipelineSize = 2000, numCommands = 1000000, successCount = 0;
+        long start = System.currentTimeMillis();
+
+        Future<RedisCommandInfo> [] futures = new Future[pipelineSize];
+        do {
+            for (i = 0; i < pipelineSize; i++) {
+                futures[i] = aredis.submitCommand(new RedisCommandInfo(RedisCommand.EVALCHECK, Script.ADD_SCRIPT, "1", "hello", "2", "world"));
+                count++;
+                if (count >= numCommands) {
+                    break;
+                }
+            }
+            for (j = 0; j < i; j++) {
+                String result = (String) futures[j].get().getResult();
+                if ("1".equals(result)) {
+                    successCount++;
+                } else if (!"0".equals(result)) {
+                    System.out.println("Invalid Result: " + result);
+                }
+            }
+        } while (count < numCommands);
+
+        long diff = System.currentTimeMillis() - start;
+        System.out.println("numCommands = " + numCommands + " time = " + diff + " rps = " + numCommands * 1000 / diff + " successful Locks = " + successCount);
+    }
+
+    // Below is the performance test of ADD_SCRIPT as main1. But the EVALSHA command is used with the sha1
+    // digest instead of the script object. This is to compare with main1 which checks the script flags
+    // whereas this does not access the Script Flags. The performance of main1 with the check is found to
+    // be close to the below main2 (Within 98% on an average).
+    public static void main2(String [] args) throws Exception {
+        AsyncRedisFactory f = new AsyncRedisFactory(null);
+        // The below call returns the same AsyncRedisClient for all calls to the same server
+        AsyncRedisClient aredis = f.getClient("my.redis.server2");
+        int i, j, count = 0, pipelineSize = 2000, numCommands = 1000000, successCount = 0;
+        long start = System.currentTimeMillis();
+
+        Future<RedisCommandInfo> [] futures = new Future[pipelineSize];
+        String addScriptSha1 = Script.ADD_SCRIPT.getSha1sum();
+        do {
+            for (i = 0; i < pipelineSize; i++) {
+                futures[i] = aredis.submitCommand(new RedisCommandInfo(RedisCommand.EVALSHA, addScriptSha1, "1", "hello", "2", "world"));
+                count++;
+                if (count >= numCommands) {
+                    break;
+                }
+            }
+            for (j = 0; j < i; j++) {
+                String result = (String) futures[j].get().getResult();
+                if ("1".equals(result)) {
+                    successCount++;
+                } else if (!"0".equals(result)) {
+                    System.out.println("Invalid Result: " + result);
+                }
+            }
+        } while (count < numCommands);
+
+        long diff = System.currentTimeMillis() - start;
+        System.out.println("numCommands = " + numCommands + " time = " + diff + " rps = " + numCommands * 1000 / diff + " successful Locks = " + successCount);
+    }
+
+    // The below program tests handling of multiple scripts
+    public static void main3(String [] args) throws Exception {
         // Create 100 different scripts by adding spaces to ADD_SCRIPT for testing
         // Better to cleanup the server by doing SCRIPT FLUSH after this test
         String referenceScript = Script.ADD_SCRIPT.getScript();
